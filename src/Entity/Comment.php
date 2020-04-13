69a9958cdcadad345b33cf2bd056b8a8ec3de990
manu <napestershine@gmail.com>
2020-04-13 20:15:43 +0200

    add a blog post comment

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -55,6 +55,7 @@ class Comment implements AuthoredEntityInterface, PublishedDateEntityInterface
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\BlogPost", inversedBy="comments")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({"post"})
      */
     private $blogPost;
 
